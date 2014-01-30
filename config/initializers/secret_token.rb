84af6c78b6493ab3b5db2081ee5eafef81d3eafc
Doug+Tom <Doug+Tom@appacademy.io>
2014-01-30 09:14:04 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Reddit::Application.config.secret_token = 'bae9683b5d122d381ae780e8e6ea443d1473e9de07ad5fab081a8911df17a9817e2cd35fcb4bb9e52a403fecffe87247e58ae97a82b8256c095195d2c3527d15'
